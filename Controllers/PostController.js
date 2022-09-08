b3078b0723861568b1d5b8108aaaf502b15d0eb4
ashishjaiswal123 <jaiswalashish025@gmail.com>
2022-09-09 02:55:05 +0530

    update and delete a post controller created in PostController

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -24,3 +24,42 @@ export const getPost = async(req, res) => {
         res.status(500).json(error)
     }
 }
+
+// Update a post 
+export const updatePost = async(req,res) => {
+    const postId = req.params.id
+    const {userId} = req.body
+
+    try {
+        const post = await PostModel.findById(postId)
+
+        if(post.userId === userId){
+            await post.updateOne({$set: req.body})
+            res.status(200).json("Post updated")
+        }else{
+            res.status(403).json("Action forbidden")
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// Delete a post
+export const deletePost = async(req, res) => {
+    const id = req.params.id
+    const {userId} = req.body
+
+    try {
+        const post = await PostModel.findById(id)
+        if(post.userId === userId){
+            await post.deleteOne()
+            // await PostModel.findByIdAndDelete(id)
+            res.status(200).json("Post deletd successfully")
+        }else{
+            res.status(403).json("Action forbidden")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
