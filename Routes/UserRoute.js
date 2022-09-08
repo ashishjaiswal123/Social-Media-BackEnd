a7da3b094877b2b3d9926c24063925c57218832a
ashishjaiswal123 <jaiswalashish025@gmail.com>
2022-09-08 20:17:08 +0530

    get a user & update a user controller created in userController

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,8 +1,9 @@
 import Express from "express";
-import { getUser } from "../Controllers/UserController.js";
+import { getUser, updateUser } from "../Controllers/UserController.js";
 
 const router = Express.Router()
 
 router.get('/:id', getUser)
+router.put('/:id', updateUser)
 
 export default router
