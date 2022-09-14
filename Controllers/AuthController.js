50db1c0924f5a4090ec3d676c3a9316f622a9844
ashishjaiswal123 <jaiswalashish025@gmail.com>
2022-09-14 21:40:27 +0530

    implementing jwt token for login&signup function in AuthController

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,23 +1,31 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 // Registering a new user
 export const registerUser = async (req, res) => {
-  const { username, password, firstname, lastname } = req.body;
+  // const { username, password, firstname, lastname } = req.body;
 
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword
+  const newUser = new UserModel(req.body);
 
-  const newUser = new UserModel({
-    username,
-    password: hashedPassword,
-    firstname,
-    lastname,
-  });
+  const {username} = req.body
 
   try {
-    await newUser.save();
-    res.status(200).json(newUser);
+    const oldUser = await UserModel.findOne({username})
+    if(oldUser){
+      return res.status(400).json({message: "username is already exists"})
+    }
+    const user = await newUser.save();
+
+    // jwt token
+    const token = jwt.sign({
+      username: user.username, id: user._id
+    }, process.env.JWT_KEY, {expiresIn: '1h'})
+
+    res.status(200).json({user, token});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -34,7 +42,15 @@ export const loginUser = async (req, res) => {
         if(user){
             const validity = await bcrypt.compare(password, user.password)
 
-            validity ? res.status(200).json(user) : res.status(400).json("wrong password or username")
+            // validity ? res.status(200).json(user) : res.status(400).json("wrong password or username")
+            if(!validity){
+              res.status(400).json("wrong password")
+            }else{
+              const token = jwt.sign({
+                username: user.username, id: user._id
+              }, process.env.JWT_KEY, {expiresIn: '1h'})
+              res.status(200).json({user, token})
+            }
         }
         else{
             res.status(404).json("user does not exists")
